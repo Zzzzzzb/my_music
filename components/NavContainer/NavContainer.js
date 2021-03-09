3da6a6994b25f46a5e1db81f2114358375c78233
zhengzb <990140299@qq.com>
2021-03-09 19:10:42 +0800

    ssss

---
+++ components/NavContainer/NavContainer.js
@@ -0,0 +1,30 @@
+// components/NavContainer.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: { // 属性名
+      type: String,
+      value: '推荐歌曲'
+    },
+    headerTitle: { // 属性名
+      type: String,
+      value: '为你精心推荐'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
